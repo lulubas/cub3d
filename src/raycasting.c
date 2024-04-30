82279ba728b691217b10a8ad9eb1e60fddc5ae39
louis <email@example.com>
2024-04-30 19:00:31 +0200

    adding ray's movement

--- src/raycasting.c
+++ src/raycasting.c
@@ -6,7 +6,7 @@
 /*   By: lbastien <lbastien@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/25 17:39:01 by lbastien          #+#    #+#             */
-/*   Updated: 2024/04/30 18:54:02 by lbastien         ###   ########.fr       */
+/*   Updated: 2024/04/30 19:00:15 by lbastien         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,8 +38,27 @@ void	raycast_and_render(t_data *data)
 		else
 			data->deltaDistY = fabs(1 / data->rayDirY);
 
-		//Determine ray grid direction
-		
+		//Determine ray steps direction in the grid
+		if (data->rayDirX < 0)
+		{
+			data->stepX = -1;
+			data->sideDistX = (data->posX - data->mapX) * data->deltaDistX;
+		}
+		else
+		{
+			data->stepX = -1;
+			data->sideDistX = (data->mapX + 1 - data->posX) * data->deltaDistX;
+		}
+		if (data->rayDirY < 0)
+		{
+			data->stepY = -1;
+			data->sideDistY = (data->posY - data->mapY) * data->deltaDistY;
+		}
+		else
+		{
+			data->stepY = 1;
+			data->sideDistY = (data->mapY + 1.0 - data->posY) * data->deltaDistY;
+		}
 	}
 
 }
